997b805097ee04f3a44e04b607b914844af776af
k-shreehari7 <hari18031911@gmail.com>
2022-03-23 19:09:56 +0530

    first commit

--- lib/story.dart
+++ lib/story.dart
@@ -1,4 +1,20 @@
 //TODO: Step 2 - Create a new class called Story.
+class Story
+{
+  String title;
+  String choice1;
+  String choice2;
+
+  Story(String t,String op1,String op2)
+  {
+    title=t;
+    choice1=op1;
+    choice2=op2;
+  }
+
+
+
+}
 
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
 
